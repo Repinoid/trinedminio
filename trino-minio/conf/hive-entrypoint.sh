fb53cc7da2a954d659a95aff7fc20d3af7306c63
“Naeel” <tazet@narod.ru>
2025-09-13 07:21:54 +0300

    hive entry comments

--- trino-minio/conf/hive-entrypoint.sh
+++ trino-minio/conf/hive-entrypoint.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
 
+# hive-metastore:
+#     image: 'bitsondatadev/hive-metastore:latest'
+#       ...
+#     volumes:
+#       - ./conf/metastore-site.xml:/opt/apache-hive-metastore-3.0.0-bin/conf/metastore-site.xml:ro
+#       - ./mariam/mariadb-java-client-3.5.6.jar:/opt/hadoop-3.2.0/share/hadoop/common/lib/mariadb-java-client-3.5.6.jar:ro
+#       - ./conf/hive-entrypoint.sh:/hive-entrypoint.sh:ro
+    #  environment:
+    #    METASTORE_DB_HOSTNAME: mariadb
+    #    DB_PORT: 3306
+
+#  указывает путь к установленному Hadoop.
 export HADOOP_HOME=/opt/hadoop-3.2.0
-export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar:${HADOOP_HOME}/share/hadoop/common/lib/*
+
+# задает класс-путь для Hadoop, включающий библиотеки AWS и другие необходимые JAR-файлы.
+export HADOOP_CLASSPATH="
+${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:\
+${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar:\
+${HADOOP_HOME}/share/hadoop/common/lib/*"
+
+# указывает на установленную версию Java.
 export JAVA_HOME=/usr/local/openjdk-8
+# задает имя хоста базы данных метастора, по умолчанию localhost , если переменная не задана.
 export METASTORE_DB_HOSTNAME=${METASTORE_DB_HOSTNAME:-localhost}
 
 echo "Waiting for database on ${METASTORE_DB_HOSTNAME} to launch on 3306 ..."
 
 # Ожидаем доступности MariaDB
+# -z означает "сканировать только наличие открытого порта без отправки данных".
+# команда проверяет, слушает ли на этом порту удаленный сервер.
 while ! nc -z ${METASTORE_DB_HOSTNAME} 3306; do
   sleep 1
 done
